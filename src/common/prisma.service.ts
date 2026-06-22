46f239b08a6cc40ce57c687c33217bd846eddc0d
nelly439 <nebenezer86@gmail.com>
2026-06-22 20:01:40 +0100

    test: make PrismaService typing permissive to accommodate test mocks and schema additions

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -2,7 +2,8 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService extends (PrismaClient as any) implements OnModuleInit, OnModuleDestroy {
+  [key: string]: any;
   async onModuleInit() {
     await this.$connect();
   }
